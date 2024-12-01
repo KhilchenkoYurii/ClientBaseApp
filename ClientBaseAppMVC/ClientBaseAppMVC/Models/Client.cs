94e7791d582f63cf04cc887f509935f65a816374
Khilchenko Yurii <yu.kh414@gmail.com>
2024-12-01 02:58:22 +0200

    project init

---
+++ ClientBaseAppMVC/ClientBaseAppMVC/Models/Client.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClientBaseAppMVC.Models
+{
+    public class Client
+    {
+        public string? Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string Surname { get; set; } = null!;
+
+        [Required]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string Phone { get; set; } = null!;
+
+        public string Address { get; set; } = null!;
+    }
+}
