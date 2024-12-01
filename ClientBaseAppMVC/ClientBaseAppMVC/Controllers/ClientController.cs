3e17d914bd5fb2ea845147be3faf7ef97ccc0440
Khilchenko Yurii <yu.kh414@gmail.com>
2024-12-01 04:52:13 +0200

    add showing Clients logic, rework db logic

--- ClientBaseAppMVC/ClientBaseAppMVC/Controllers/ClientController.cs
+++ ClientBaseAppMVC/ClientBaseAppMVC/Controllers/ClientController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ClientBaseAppMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using ClientBaseAppMVC.Data;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace ClientBaseAppMVC.Controllers
 {
     public class ClientController : Controller
     {
+
+        private readonly MongoDbContext _context;
+
+        public ClientController(IOptions<ClientsDatabaseSettings> settings)
+        {
+            _context = new MongoDbContext(settings);
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Client> objList = _context.Clients;
+
+            return View(objList);
         }
     }
 }
---
+++ ClientBaseAppMVC/ClientBaseAppMVC/Data/MongoDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ClientBaseAppMVC.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace ClientBaseAppMVC.Data
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(IOptions<ClientsDatabaseSettings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            _database = client.GetDatabase(settings.Value.DatabaseName);
+        }
+
+        public List<Client> Clients =>
+            _database.GetCollection<Client>("Clients").AsQueryable<Client>().ToList<Client>();
+    }
+}
